26e41266545932b949c06376cce577a1b8a921a0
XiangLuc <xiangluc01@gmail.com>
2025-03-06 14:03:29 +0100

    init commit

---
+++ src/test/java/org/univparis8/iut/montreuil/gestionnaireconge/GestionnaireCongeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.univparis8.iut.montreuil.gestionnaireconge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionnaireCongeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
